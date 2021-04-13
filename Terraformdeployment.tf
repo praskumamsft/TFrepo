4b3d0276d5267a030b2a612c7be165af719cdc74
praskumamsft <praskuma@microsoft.com>
2021-04-13 17:54:11 +0530

    adding TF files

---
+++ Terraformdeployment.tf
@@ -0,0 +1,65 @@
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resourcghhhhhhhh"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "${var.adminuser}"
+  admin_password      = "${var.adminpassword}"
+  disable_password_authentication="false"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "adminuser" {
+  description = "The admin user name"
+  type        = string
+  default     = "Azureadmin"
+}
+
+variable "adminpassword" {
+  description = "The admin password"
+  type        = string
+  default     = "Azure@12345678910"
+}
